d62393c14d65fc83a048a78c70c7273bef340db5
kimi casamina <kimi.casamina@gmail.com>
2023-07-08 14:09:52 +0800

    feat: added active states on chevron menu

--- src/Container/TopContainer/TopContainer.jsx
+++ src/Container/TopContainer/TopContainer.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import {FaSistrix, FaBell, FaChevronDown} from 'react-icons/fa'
 import Avatar from '../../Assets/Images/pinacolada.jpg'
 
 const TopContainer = () => {
+    const menuContainer = useRef(null)
+
+    // const handleChevronEnter = (e) => {
+    //     // console.log(menuContainer.current)
+    //     // console.log(e.currentTarget)
+    //     if (!menuContainer.current.classList.contains('active')){
+    //         menuContainer.current.classList.add('active')
+    //         // console.log("ACTIVE")
+    //     }
+    // }
+
+    // const handleChevronLeave = (e) => {
+    //     if (menuContainer.current.classList.contains('active')){
+    //         setTimeout(() => {
+    //             menuContainer.current.classList.remove('active')
+    //             // console.log('REMOVED ACTIVE')
+    //         }, 1000)
+    //     }
+    // }
+
+    const handleMenuEnter = () => {
+        if (!menuContainer.current.classList.contains('active')){
+            menuContainer.current.classList.add('active')
+            console.log("ACTIVE")
+        }
+    }
+
+    const handleMenuLeave = () => {
+        if (menuContainer.current.classList.contains('active')){
+            setTimeout(() => {
+                menuContainer.current.classList.remove('active')
+                console.log('REMOVED ACTIVE')
+            }, 1000)
+        }
+    }
+
   return (
     <div className="topContainer">
         <div className="inputBox">
@@ -18,11 +54,11 @@ const TopContainer = () => {
             </i>
             <img className="profileImage" src={Avatar} alt="" />
             <p className="profileName">Pinacolada</p>
-            <i className="chevronMenu">
+            <i className="chevronMenu" onMouseEnter={(e) => {handleMenuEnter(e)}} onMouseLeave={(e) => {handleMenuLeave(e)}}>
                 <FaChevronDown />
             </i>
 
-            <div className="chevronMenuContainer">
+            <div className="chevronMenuContainer" ref={menuContainer} onMouseOver={(e) => {handleMenuEnter(e)}} onMouseLeave={(e) => {handleMenuLeave(e)}}>
                 <ul>
                     <li>Web Design</li>
                     <li>UI/UX</li>
