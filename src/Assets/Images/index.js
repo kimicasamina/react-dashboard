b2161434db25fa3a6f618ea1f8a2366d2055b5dc
kimi casamina <kimi.casamina@gmail.com>
2023-07-09 13:46:12 +0800

    feat: added statistics and topsellers card on the right container

--- src/Assets/Images/index.js
+++ src/Assets/Images/index.js
@@ -6,7 +6,12 @@ const images = {
     light: require('./light.jpg'),
     mountain: require('./moutain.jpg'),
     tinyhome: require('./tinyhome.jpg'),
-    waves: require('./waves.jpg')
+    waves: require('./waves.jpg'),
+    avatar0: require('./avatar.jpg'),
+    avatar1: require('./avatar1.jpg'),
+    avatar2: require('./avatar2.jpg'),
+    avatar3: require('./avatar3.jpg'),
+    avatar4: require('./avatar4.jpg')
 }
 
 export default images
--- src/Container/MainContainer/RightContainer/BottomRightContainer/Index.jsx
+++ src/Container/MainContainer/RightContainer/BottomRightContainer/Index.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
+import images from '../../../../Assets/Images'
 
 const Index = () => {
   return (
     <div className="bottomRightContainer">
-      Bottom right container
+      <div className="topSellers">
+        <div className="topSellers__name">
+          <h3>Top Seller</h3>
+          <a href="#">View More</a>
+        </div>
+        <div className="topSellers__list">
+          <li className="profile">
+            <img src={images.avatar0} alt="" />
+            <div className="profile__name">
+              <h3>Jacquelyn Schaefer</h3>
+              <p>@jschaefere</p>
+            </div>
+            <button className="btn">FOLLOW</button>
+          </li>
+          <li className="profile">
+            <img src={images.avatar1} alt="" />
+            <div className="profile__name">
+              <h3>Haleigh Howell</h3>
+              <p>@hhowell</p>
+            </div>
+            <button className="btn">FOLLOW</button>
+          </li>
+          <li className="profile">
+            <img src={images.avatar2} alt="" />
+            <div className="profile__name">
+              <h3>Haleigh Howell</h3>
+              <p>@hhowell</p>
+            </div>
+            <button className="btn">FOLLOW</button>
+          </li>
+          <li className="profile">
+          <img src={images.avatar3} alt="" />
+            <div className="profile__name">
+              <h3>Breana Bartoletti</h3>
+              <p>@breanaBaratoletti</p>
+            </div>
+            <button className="btn">FOLLOW</button>
+          </li>
+          <li className="profile">
+            <img src={images.avatar4} alt="" />
+            <div className="profile__name">
+              <h3>Toby Sporer</h3>
+              <p>@tobysporer</p>
+            </div>
+            <button className="btn">FOLLOW</button>
+          </li>
+          
+        </div>
+      </div>
     </div>
   )
 }
